c70c3753e39012e46aa66032058cc5c7e0fc7e9c
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-03-29 15:55:14 +0900

    tailwindCSS適応

--- src/resources/js/Pages/Top/index.tsx
+++ src/resources/js/Pages/Top/index.tsx
@@ -4,6 +4,9 @@ export const Top = React.memo(function Top() {
     return (
         <>
             TOP
+            <div className="bg-green-600">
+                green
+            </div>
         </>
     );
 })
