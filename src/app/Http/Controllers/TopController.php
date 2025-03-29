ae1d9a53c804080af8406e471327cbe19f37f375
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-03-29 15:45:30 +0900

    フロントの設定

--- src/app/Http/Controllers/TopController.php
+++ src/app/Http/Controllers/TopController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class TopController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Pages/TopView');
+        return Inertia::render('Top');
     }
 }
---
+++ src/resources/js/Pages/Top/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Top = React.memo(function Top() {
+    return (
+        <>
+            TOP
+        </>
+    );
+})
+export default Top
